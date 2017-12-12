2537330cab662fb0931551f10687e4fdee88234b
t_nakajima <t_nakajima@bbmedia.co.jp>
2017-12-12 12:42:39 +0900

    ダミーデータ挿入用seeder作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,16 +13,13 @@ class DatabaseSeeder extends Seeder
     {
         $faker = \Faker\Factory::create();
 
-        $user = new \App\User();
-        $user->name = $faker->name;
-        $user->email = $faker->unique()->safeEmail;
-        $user->password = bcrypt('password');
-        $user->remember_token = str_random(10);
-        $user->save();
+        $holiday = new \App\Holiday();
+        $holiday->id = $faker->id;
+        $holiday->name = $faker->name;
+        $holiday->year = $faker->year;
+        $holiday->month = $faker->month;
+        $holiday->day = $faker->dayOfMonth;
+        $holiday->save();
 
-        $item = new \App\Item();
-        $item->user_id = $user->id;
-        $item->content = $faker->text();
-        $item->save();
     }
 }
