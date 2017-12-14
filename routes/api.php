329d8af73e6af853d9c4219bfd68c4cb2ac32b3c
t_nakajima <t_nakajima@bbmedia.co.jp>
2017-12-14 18:20:58 +0900

    微修正

--- routes/api.php
+++ routes/api.php
@@ -17,19 +17,24 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
+
+
 //Route::pattern('apiVersion2', 'v2');
 Route::group(['namespace' => 'Api\V2'], function() {
-    Route::resource('holidays', 'HolidayController', ['except' => ['create', 'edit', 'show']]);
+    Route::resource('/v2/holidays', 'HolidayController', ['except' => ['create', 'edit', 'show']]);
     Route::get('/v2/holidays/{id}', 'HolidayController@show');
 });
+
 
 //Route::pattern('apiVersion1', 'v[12]');
 //Route::pattern('id', '[0-9]+');
 
 Route::group(['namespace' => 'Api\V1'], function () {
-    Route::resource('holidays', 'HolidayController', ['except' => ['create', 'edit', 'show']]);
+    Route::resource('/v1/holidays', 'HolidayController', ['except' => ['create', 'edit', 'show']]);
     Route::get('/v1/holidays/{id}', 'HolidayController@show');
 });
+
+
 
 //Route::resource('holidays', 'HolidayController', ['except' => ['create', 'edit', 'show']]);
 //Route::get('/holidays/{id}', 'HolidayController@show');
